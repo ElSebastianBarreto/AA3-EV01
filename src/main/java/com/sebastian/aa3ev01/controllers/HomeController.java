5e5c4a0a247f1253e5834c3882aa9dc17387b119
ElSebastianBarreto <sebastian.barreto.rojas@unillanos.edu.co>
2024-07-23 00:11:31 -0500

    Luego de login muestra listado de clientes

--- src/main/java/com/sebastian/aa3ev01/controllers/HomeController.java
+++ src/main/java/com/sebastian/aa3ev01/controllers/HomeController.java
@@ -1,17 +1,26 @@
 package com.sebastian.aa3ev01.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sebastian.aa3ev01.models.Cliente;
+import com.sebastian.aa3ev01.repositories.*;
 
 @Controller
 public class HomeController {
-	
+	@Autowired
+	private ClientesRepository repo;
 	@GetMapping({"", "/"})
-	public String home() {
+public String showClientList(Model model) {
 		
 		
+		List<Cliente> clientes = repo.findAll();
+		model.addAttribute("cliente", clientes);
 		return "index";
-
 	}
 
 	
