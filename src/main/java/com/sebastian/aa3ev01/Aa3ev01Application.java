bcd2d11cfdd3f75effb850aa893f787736e06f10
ElSebastianBarreto <sebastian.barreto.rojas@unillanos.edu.co>
2024-07-22 22:44:34 -0500

    nuevo commit

---
+++ src/main/java/com/sebastian/aa3ev01/Aa3ev01Application.java
@@ -0,0 +1,13 @@
+package com.sebastian.aa3ev01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa3ev01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aa3ev01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebastian/aa3ev01/Aa3ev01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.aa3ev01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aa3ev01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
