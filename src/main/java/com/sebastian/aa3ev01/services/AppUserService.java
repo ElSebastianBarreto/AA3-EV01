2e96e3ae07bc6de8f5a2b9988d9df0d7263e7f22
ElSebastianBarreto <sebastian.barreto.rojas@unillanos.edu.co>
2024-07-22 23:45:39 -0500

    implementacion de login con credenciales de base de datos

--- src/main/java/com/sebastian/aa3ev01/services/AppUserService.java
+++ src/main/java/com/sebastian/aa3ev01/services/AppUserService.java
@@ -1,5 +1,35 @@
 package com.sebastian.aa3ev01.services;
 
-public class AppUserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.sebastian.aa3ev01.models.Cliente;
+import com.sebastian.aa3ev01.repositories.ClientesRepository;
+
+
+
+@Service
+public class AppUserService implements UserDetailsService {
+
+	@Autowired
+	private ClientesRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String correo) throws UsernameNotFoundException{ 
+		
+		Cliente  appUser = repo.findByCorreo(correo);
+		
+		if(appUser != null) {
+			
+			var springUser = User.withUsername(appUser.getCorreo())
+					.password(appUser.getClave())
+				
+					.build();
+			return springUser;
+		}
+		return null;}
 }
